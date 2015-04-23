c76aafdfe6bfc893db3a1d36c3797b97defb878a
Hayley Swimelar <hswimelar@gmail.com>
2015-04-22 17:01:05 -0700

    add linting of app.js to gulp file

--- front_end/gulpfile.js
+++ front_end/gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
-    console.log('Gulp is running...');
+var jslint = require('gulp-jslint'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+// Lint our javascript.
+gulp.task('lint', function () {
+    return gulp.src(['app/app.js'])
+        .pipe(jslint({
+            brower: true,
+            maxlen: 80,
+            predef: ['angular', '$'],
+            errorsOnly: false
+        }))
+        .on('error', function (error) {
+            console.error(String(error));
+        });
 });
+
+gulp.task('default', ['lint']);
