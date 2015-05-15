d24e035c0ad89f0c2dcfe90ec046e473291b7001
Hayley Swimelar <hswimelar@gmail.com>
2015-05-14 19:11:33 -0700

    add headerService

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -669,3 +669,203 @@
             };
         });
 }());
+
+/*global angular, $ */
+
+(function () {
+    'use strict';
+    angular.module('portfolio', [
+        'headerDirective',
+        'footerDirective',
+        'cardDirective',
+        'navbarDirective'
+    ]);
+}());
+
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('footerDirective', []).
+        directive('portfolioFooter', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/footer/footerView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('headerDirective', []).
+        directive('portfolioHeader', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/header/headerView.html'
+            };
+        });
+}());
+
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('navbarDirective', []).
+        directive('portfolioNavbar', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/navbar/navbarView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('cardDirective', []).
+        directive('card', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/shared/cards/cardView.html'
+            };
+        });
+}());
+
+/*global angular, $ */
+
+(function () {
+    'use strict';
+    angular.module('portfolio', [
+        'headerDirective',
+        'footerDirective',
+        'cardDirective',
+        'navbarDirective'
+    ]);
+}());
+
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('footerDirective', []).
+        directive('portfolioFooter', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/footer/footerView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('navbarDirective', []).
+        directive('portfolioNavbar', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/navbar/navbarView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('headerDirective', []).
+        directive('portfolioHeader', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/header/headerView.html'
+            };
+        });
+}());
+
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('cardDirective', []).
+        directive('card', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/shared/cards/cardView.html'
+            };
+        });
+}());
+
+/*global angular, $ */
+
+(function () {
+    'use strict';
+    angular.module('portfolio', [
+        'headerDirective',
+        'footerDirective',
+        'cardDirective',
+        'navbarDirective'
+    ]);
+}());
+
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('footerDirective', []).
+        directive('portfolioFooter', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/footer/footerView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('headerDirective', []).
+        directive('portfolioHeader', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/header/headerView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('headerService', []).
+        factory('Header', function HeaderFactory() {
+            var api = '../dummy_json/',
+                headerData = 'header.json';
+            return {
+                function(api, headerData) {
+                    return api + headerData;
+                }
+            }
+}());
+
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('navbarDirective', []).
+        directive('portfolioNavbar', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/components/navbar/navbarView.html'
+            };
+        });
+}());
+
+/*global angular*/
+(function () {
+    'use strict';
+    angular.module('cardDirective', []).
+        directive('card', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/shared/cards/cardView.html'
+            };
+        });
+}());
