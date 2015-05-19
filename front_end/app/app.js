ecdc064eac8bd429b1642301a694b039c805d2e2
Hayley Swimelar <hswimelar@gmail.com>
2015-05-19 09:58:28 -0700

    remove headerService

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -56,20 +56,6 @@
 /*global angular*/
 (function () {
     'use strict';
-    angular.module('headerService', []).
-        factory('Header', ['$http', function HeaderFactory($http) {
-            var api = 'http://hayswim.com/dummy_json/',
-                headerData = 'header.json';
-            return function () {
-                return $http.get(api + headerData);
-            };
-        }]);
-}());
-
-
-/*global angular*/
-(function () {
-    'use strict';
     angular.module('navbarDirective', []).
         directive('portfolioNavbar', function () {
             return {
