eda9a1b1565943f76ec5303c542f8bb4f26afb7f
Hayley Swimelar <hswimelar@gmail.com>
2015-05-20 08:17:40 -0700

    add routes to router

--- front_end/app/routes.js
+++ front_end/app/routes.js
@@ -3,7 +3,25 @@
     'use strict';
     angular.module('portfolio').
         config(function($routeProvider) {
-
+        $routeProvider.
+            when('/projects', {
+            templateUrl: 'app/shared/cards/cardView.html'
+        }).
+            when('/technologies', {
+            templateUrl: 'app/shared/cards/cardView.html'
+        }).
+            when('/jobs', {
+            templateUrl: 'app/shared/cards/cardView.html'
+        }).
+            when('/schools', {
+            templateUrl: 'app/shared/cards/cardView.html'
+        }).
+            when('/', {
+            templateUrl: 'app/shared/cards/cardView.html'
+        }).
+            otherwise({
+            redirectTo: '/'
+        });
     });
 }());
 
