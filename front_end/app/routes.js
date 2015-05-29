07083c098e1607eea7a76ce047206804d796dea3
Hayley Swimelar <hswimelar@gmail.com>
2015-05-28 17:08:44 -0700

    clean up spacing in routes.js

--- front_end/app/routes.js
+++ front_end/app/routes.js
@@ -2,37 +2,37 @@
 (function () {
     'use strict';
     angular.module('portfolio').
-        config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider.
-            when('/projects', {
-            templateUrl: 'app/shared/cards/projects/cardView.html',
-            controller: 'projectsController',
-            controllerAs: 'projectsArray'
-        }).
-            when('/technologies', {
-            templateUrl: 'app/shared/cards/technologies/cardView.html',
-            controller: 'technologiesController',
-            controllerAs: 'technologiesArray'
-        }).
-            when('/jobs', {
-            templateUrl: 'app/shared/cards/jobs/cardView.html',
-            controller: 'jobsController',
-            controllerAs: 'jobsArray'
-        }).
-            when('/schools', {
-            templateUrl: 'app/shared/cards/schools/cardView.html',
-            controller: 'schoolsController',
-            controllerAs: 'schoolsArray'
-        }).
-            when('/', {
-            templateUrl: 'app/shared/cards/projects/cardView.html',
-            controller: 'projectsController',
-            controllerAs: 'projectsArray'
-        }).
-            otherwise({
-            redirectTo: '/'
+        config(function ($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+            $routeProvider.
+                when('/projects', {
+                    templateUrl: 'app/shared/cards/projects/cardView.html',
+                    controller: 'projectsController',
+                    controllerAs: 'projectsArray'
+                }).
+                    when('/technologies', {
+                    templateUrl: 'app/shared/cards/technologies/cardView.html',
+                    controller: 'technologiesController',
+                    controllerAs: 'technologiesArray'
+                }).
+                    when('/jobs', {
+                    templateUrl: 'app/shared/cards/jobs/cardView.html',
+                    controller: 'jobsController',
+                    controllerAs: 'jobsArray'
+                }).
+                    when('/schools', {
+                    templateUrl: 'app/shared/cards/schools/cardView.html',
+                    controller: 'schoolsController',
+                    controllerAs: 'schoolsArray'
+                }).
+                    when('/', {
+                    templateUrl: 'app/shared/cards/projects/cardView.html',
+                    controller: 'projectsController',
+                    controllerAs: 'projectsArray'
+                }).
+                    otherwise({
+                    redirectTo: '/'
+                });
         });
-    });
 }());
 
