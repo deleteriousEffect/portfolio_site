8abc2a9e6c911d487aa8c73b29606fc92a6d7fae
Hayley Swimelar <hswimelar@gmail.com>
2015-05-28 17:09:53 -0700

    clean up spacing in headerController.js

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -70,14 +70,14 @@
         controller('headerController', ['$http', function ($http) {
             var vm = this;
             $http.get('http://api.hayswim.com/headers/1')
-            .success(function(data) {
-                console.log(data);
-                var headerData = data.headers[0].header;
-                vm.title = headerData.title;
-                vm.subtitle = headerData.subtitle;
-                vm.iconUrl = headerData['profile picture'].src;
-                vm.altText = headerData['profile picture'].alt;
-            });
+                .success(function (data) {
+                    console.log(data);
+                    var headerData = data.headers[0].header;
+                    vm.title = headerData.title;
+                    vm.subtitle = headerData.subtitle;
+                    vm.iconUrl = headerData['profile picture'].src;
+                    vm.altText = headerData['profile picture'].alt;
+                });
         }]);
 }());
 
--- front_end/app/components/header/headerController.js
+++ front_end/app/components/header/headerController.js
@@ -5,13 +5,13 @@
         controller('headerController', ['$http', function ($http) {
             var vm = this;
             $http.get('http://api.hayswim.com/headers/1')
-            .success(function(data) {
-                console.log(data);
-                var headerData = data.headers[0].header;
-                vm.title = headerData.title;
-                vm.subtitle = headerData.subtitle;
-                vm.iconUrl = headerData['profile picture'].src;
-                vm.altText = headerData['profile picture'].alt;
-            });
+                .success(function (data) {
+                    console.log(data);
+                    var headerData = data.headers[0].header;
+                    vm.title = headerData.title;
+                    vm.subtitle = headerData.subtitle;
+                    vm.iconUrl = headerData['profile picture'].src;
+                    vm.altText = headerData['profile picture'].alt;
+                });
         }]);
 }());
