bd81dbd893185b6fe5b5581e850e8379496464bd
Hayley Swimelar <hswimelar@gmail.com>
2015-06-26 13:20:25 -0700

    clean up structure

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -40,6 +40,35 @@
         });
 }());
 
+/*global angular, $*/
+(function () {
+    'use strict';
+    angular.module('portfolio.service', [])
+        .service('portfolioService', ['$http', '$q', function ($http, $q) {
+            var API_ENDPOINT = 'http://api.hayswim.com';
+
+            function getPortfolioData(section) {
+                var deferred = $q.defer();
+
+                $http.get(API_ENDPOINT + '/' + section)
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    })
+                    .error(function () {
+
+                        deferred.resolve({
+                            errorMessage: "Connection lost, please reload page or return later."
+                        });
+                    });
+                return deferred.promise;
+            }
+            return {
+                getPortfolioData: getPortfolioData
+            };
+
+        }]);
+}());
+
 /*global angular, $ */
 (function () {
     'use strict';
@@ -162,35 +191,6 @@
                         }
                     }
                 });
-        }]);
-}());
-
-/*global angular, $*/
-(function () {
-    'use strict';
-    angular.module('portfolio.service', [])
-        .service('portfolioService', ['$http', '$q', function ($http, $q) {
-            var API_ENDPOINT = 'http://api.hayswim.com';
-
-            function getPortfolioData(section) {
-                var deferred = $q.defer();
-
-                $http.get(API_ENDPOINT + '/' + section)
-                    .success(function (data) {
-                        deferred.resolve(data);
-                    })
-                    .error(function () {
-
-                        deferred.resolve({
-                            errorMessage: "Connection lost, please reload page or return later."
-                        });
-                    });
-                return deferred.promise;
-            }
-            return {
-                getPortfolioData: getPortfolioData
-            };
-
         }]);
 }());
 
