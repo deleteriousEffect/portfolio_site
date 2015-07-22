b4c1d59116d11f9dbae08c61b214548665edd989
Hayley Swimelar <hswimelar@gmail.com>
2015-07-22 15:34:51 -0700

    remove console.log

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -297,7 +297,6 @@
         .controller('jobsController', function (jobsData) {
             var vm = this;
             vm.jobs = jobsData.jobs;
-            console.log(vm.jobs[0].responsibility.split('\\'));
             vm.errorMessage = jobsData.errorMessage;
             vm.startDateSort = function (job) {
                 return new Date(job.start_date);
